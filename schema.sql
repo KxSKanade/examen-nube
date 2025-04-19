504e1173a8dea5214b3d3d00950f44fb93bb4437
KxSKanade <terry.lazaro@tecsup.edu.pe>
2025-04-19 09:38:47 -0500

    muha

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE personas (
-    id SERIAL PRIMARY KEY,
-    dni VARCHAR(20) NOT NULL,
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR(100) NOT NULL,
-    direccion TEXT,
-    telefono VARCHAR(20)
-);
